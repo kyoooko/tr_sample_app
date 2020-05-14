2c07eb9af93104284147f9747324c6375659e189
vagrant <vagrant@localhost.localdomain>
2020-05-14 08:52:38 +0000

    [5-3] rewrite_by_scss & use 名前付きルート

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,34 +7,40 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    # get root_urlを書き換え
+    get root_path
     assert_response :success
     # assert_select "title", "Home | #{@base_title}"
   end
 
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
-  end
+  # test "should get home" do
+  #   get static_pages_home_url
+  #   assert_response :success
+  #   assert_select "title", "Ruby on Rails Tutorial Sample App"
+  # end
 
   test "should get help" do
-    get static_pages_help_url
+    # get static_pages_help_urlを書き換え
+    get help_path
+    # 演習 get helf_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    # get static_pages_about_urlを書き換え
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
-  
+
   test "should get contact" do
-    get static_pages_contact_url
+    # get static_pages_contact_urlを書き換え
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  
+  # full_titleテストできるように下記追加
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
